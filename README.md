a8d12b45bc1289f77c861a22392dfa2423f67dc3
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-18 14:49:28 -0700

    created readMe format

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 
-    # Project Name
-   <h1 align ="center">jsdnjfnjsdnfjsdnf<h1>
+   <h1 align ="center">Helping Hand<h1>
+
+undefined
+
+## Description
+A project to find nearby charities based on location
+
+# Table of Contents
+* [Description](#description)
+* [Project-Use](#usage)
+* [Project-Installation](#installation)
+* [ProjectLicense](#license)
+* [Project-Contributions](#contributions)
+* [Github-Username](#github)
+
+## Project-Use
+To provide an easier way for people to find donation centers
+
+## Project-Installation
+intall via: 
+
+## ProjectLicense
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+<br>
+This Project is licensed under the MIT license.
+
+## Project-Contributions
+Myself
+
+## Github-Username
+Questions? 
+Contact me through my github Yahir-F
+
+Email: @gmail.com
+
+
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@ function readMeGenerator(response) {
     return `
    <h1 align ="center">${response.projectName}<h1>
 
+${response.license}
+
+## Description
+${response.projectDescription}
+
+# Table of Contents
+* [Description](#description)
+* [Project-Use](#usage)
+* [Project-Installation](#installation)
+* [ProjectLicense](#license)
+* [Project-Contributions](#contributions)
+* [Github-Username](#github)
+
+## Project-Use
+${response.projectUse}
+
+## Project-Installation
+${response.projectinstallation}
+
+## ProjectLicense
+![License](https://img.shields.io/badge/license-${response.projectLicense}-blue.svg)
+<br>
+This Project is licensed under the ${response.projectLicense} license.
+
+## Project-Contributions
+${response.projectContributions}
+
+## Github-Username
+Questions? 
+Contact me through my github ${response.githubUsername}
+
+Email: ${response.email}
+
+
+
 
 
 
